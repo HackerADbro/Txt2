59e83c0910801a5cd1db4ee2a9d24b2fbeef0d82
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2023-12-07 00:14:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "25567551"
 api_hash = "45993062e6b160a7a360d648ecc8e43a"
-bot_token = "6289518350:AAE07jeBHvV9kOG42mvuOU86jibTisfdPRc" 
-sudo_group = -1002120916178
+bot_token = "6507030900:AAGDgQ27SfvPrO_yA1TU5_KtuB7WRF1x8Wk" 
+sudo_group = -4050496587
